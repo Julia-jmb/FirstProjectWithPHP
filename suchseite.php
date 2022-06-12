7346c7a6311eee0e1343ba8d29a37d890e76778c
Julia-jmb <91531368+Julia-jmb@users.noreply.github.com>
2022-06-12 16:22:13 +0200

    Update suchseite.php

--- suchseite.php
+++ suchseite.php
@@ -12,17 +12,34 @@ Datum: 13.06.2022
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
-  <!-- Einbinden von Bootstrap -->
+<!-- Einbinden von Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   
-  <!-- Einbinden des Autorenstylesheets: -->
+<!-- Einbinden des Autorenstylesheets: -->
   <link rel="stylesheet" type="text/css" href="css/styles.css">
 
 </head>
 
 <body>
   
+
+
+<nav class="navbar navbar-expand-md p-2 navbar-light bg-light">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">Home</a>
+      </li>
+     
+    </ul>
+  </div>
+</nav>
+
 <header>
    <div class="container-fluid p-5 text-white text-center">
     <h1>Filmverwaltung</h1>
@@ -30,7 +47,6 @@ Datum: 13.06.2022
   </div>
 </header>
 
-<article>
   <div class="container-md">
     <div class="container mt-5">
       <div class="row">
@@ -41,10 +57,10 @@ Datum: 13.06.2022
           <form action="suchseite.php" method="POST" class="form-inline">
           <label class="sr-only" for="inlineforminput">Produktionsfirma</label>
             <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-              <input type="search" name="studioname" class="form-control" id="inlineforminput">
+              <input type="search" name="studioname"  class="form-control" id="inlineforminput">
             </div> 
               <div class="form-group">
-              <button  name="search" type="submit" class="btn mt-3 btn-info">Suche</button>
+              <button  name="studio" type="submit" value="studio" class="btn mt-3 btn-info">Suche</button>
             </div>
           </form>
   </div>
@@ -59,73 +75,49 @@ Datum: 13.06.2022
           <input type="search" name="nachname" class="form-control" id="inlineforminput">
         </div> 
         <div class="form-group">
-          <button name="search" type="submit" class="btn mt-3 btn-info">Suche</button>
+          <button name="schauspieler" type="submit" value="schauspieler" class="btn mt-3 btn-info">Suche</button>
          </div>
        </form>
           </div>
         </div>
-        </article>  
+        </div>    
+        </div>
 
-<!-- Verbindungsaufbau zur DB, über eingebundenes db-php-file -->
+ <!-- Verbindungsaufbau zur DB, über eingebundenes db-php-file bzw über die Instanz der abfrageDB-Klasse-->
+
 <?php
 require_once 'inc/db.php';
 require_once 'abfragen/abfragenDB.php';
 
-$abfrageDB = new \abfragen\AbfragenDB(); 
+$abfrageDB = new \abfragen\AbfragenDB($connection); 
 
-if(!empty($_POST["studioname"])){
-  $sucheStudio = $abfrageDB->getFilmstudios($studioname = $_POST["studioname"]);?>
-  <div class="container mt-5">
-      <div class="container-studio">
-       <div class="container md-6">
-           <h2>Suchergebnis Filmtitel</h2>
-            <div class="table-responsive-sm">          
-             <table class="table table-bordered">
-              <thead>
-                <tr>
-                 <th>Filmtitel</th>
-                 <th>Erscheinungsdatum</th>
-                 <th>Produktionsfirma</th>
-                 </tr>
-              </thead>
-              <tbody>
-<?php  
-    foreach($sucheStudio as $row){
-?>
-        <tr>
-            <td><?php  echo $row["Filmtitel"]; ?></td>
-            <td><?php echo date('d.m.Y', strtotime($row['Erscheinungsdatum'])); ?></td>
-            <td><?php echo $row['studioname'];?></td></tr>
-            </tr>
- 
-<?php 
-  }
-?> 
-          </tbody>
-        </table>
+#SUCHE PRODUKTIONSFIRMA
+
+if(!empty($_POST['studioname'])) {
+  $sucheStudio = $abfrageDB->getFilmstudios($studioname = $_POST["studioname"]);
+    if ($sucheStudio == NULL) { 
+    ?>    
+    <div class="container-md mt-5 text-center">
+      <div class="row">    
+        <div class="container md-6">
+          <h3><?php echo "Keine Einträge zu '{$studioname}' gefunden"; ?></h3>
+        </div>
       </div>
-           </div>
-         </div>
-         </div>
-
-<?php 
-} elseif (!empty($sucheStudio)) {
-    echo "Keine Einträge zu diesem Studio gefunden!";
-
-} elseif  (!empty($_POST["nachname"])){
-  $sucheSchauspieler = $abfrageDB->getSchauspieler($nachname = $_POST["nachname"]);
-?>
+    </div>
+       
+<?php    
+}else {
+  ?>
   <div class="container mt-5">
-  <div class="container-schauspieler">
+  <div class="container-studio">
     <div class="container md-6">
-    <h2>Suchergebnis Künstler:Innen</h2>
+    <h2>Suchergebnis Produktionsfirma</h2>
     <div class="table-responsive-sm">          
       <table class="table table-bordered">
         <thead>
           <tr>
-            <th>Vorname</th>
-            <th>Nachname</th>
             <th>Filmtitel</th>
+            <th>Erscheinungsdatum</th>
             <th>Produktionsfirma</th>
           </tr>
         </thead>
@@ -133,17 +125,16 @@ if(!empty($_POST["studioname"])){
         <tr>
 <?php
 #Ausgabe der Suchergebnisse in der aufgerufenen Tabelle
- foreach($sucheSchauspieler AS $row){ 
+ foreach($sucheStudio AS $row){ 
 ?>    
     <tr>
-      <td><?php echo $row['vorname'] ?></td>
-      <td><?php echo htmlspecialchars($row['nachname']) ?></td>
       <td><?php echo htmlspecialchars($row['Filmtitel'])?></td>
+      <td><?php echo date('d.m.Y', strtotime($row['Erscheinungsdatum']))?></td>
       <td><?php echo htmlspecialchars($row['studioname'])?></td></tr>
     </tr>
-<?php 
+  <?php 
   }
-?>
+  ?>
       </tbody>
       </table>
     </div>
@@ -151,44 +142,102 @@ if(!empty($_POST["studioname"])){
   </div>
   </div>
 
-    
-<?php
+<?php 
+  }
+} 
 
-} elseif (!empty($sucheSchauspieler)){
-  echo "Keine Einträge zu diesem Künstler gefunden!";
+if((empty($_POST['studioname']) AND !empty($_POST['studio'])) OR (empty($_POST['nachname']) AND !empty($_POST['schauspieler']))) {
+  ?>
+  
+      <div class="container-md mt-5 text-center">
+        <div class="row">
+          <div class="container md-12">
+            <h3><?php echo "Bitte einen Suchbegriff eingeben"; ?></h3>
+          </div>
+        </div>
+      </div>
+      <?php
+    }
+ ?>
 
-#beenden der elseif-Anweisung
-} else {
-?> 
-  <div class="container-md mt-5 text-center">
-    <div class="row">
-      <div class="container md-12">
-        <h3><?php echo "Bitte einen Suchbegriff eingeben"; ?></h3>
+
+
+
+<?php    
+# ==== SUCHE KUENSTLER:INNEN ======
+if(!empty($_POST['nachname'])) {
+  $sucheSchauspieler = $abfrageDB->getSchauspieler($nachname = $_POST["nachname"]);
+    if ($sucheSchauspieler == NULL) { 
+    ?>        
+    <div class="container-md mt-5 text-center">
+      <div class="row">
+        <div class="container md-6">
+          <h3><?php echo "Keine Einträge zu '{$nachname}' gefunden"; ?></h3>
+        </div>
       </div>
     </div>
-  </div>
- 
+       
+    <?php    
+    } else {
+    ?>
+      <div class="container mt-5">
+      <div class="container-studio">
+        <div class="container md-6">
+        <h2>Suchergebnis Künstler:Innen</h2>
+        <div class="table-responsive-sm">          
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>Filmtitel</th>
+                <th>Vorname</th>
+                <th>Nachname</th>
+                <th>Produktionsfirma</th>
+              </tr>
+            </thead>
+          <tbody>
+            <tr>
 <?php
-#beenden der else-Anweisung 
-}
+#Ausgabe der Suchergebnisse in der aufgerufenen Tabelle
+ foreach($sucheSchauspieler AS $row){ 
+?>    
+    <tr>
+      <td><?php echo htmlspecialchars($row['Filmtitel'])?></td>
+      <td><?php echo htmlspecialchars($row['vorname'])?></td>
+      <td><?php echo htmlspecialchars($row['nachname'])?></td>
+      <td><?php echo htmlspecialchars($row['studioname'])?></td>
+    </tr>
+  <?php 
+  }
+  ?>
+      </tbody>
+      </table>
+    </div>
+  </div>
+  </div>
+  </div>
+
+<?php 
+  }
+} 
 ?>
 
 
-<!-- Footer-Bereich -->
 
-<footer id="footer">
-	<div class="container-md">
-		<div class="row">
-      <div id="footer" class="container-fluid mt-5 pb-4 text-white text-center">
-       <nav id="footer-nav">
-				<a href="aboutus.html">About Us</a>
-			 </nav>	
-       <span>Funsite für Cineasten by Julia Beck (2022)</span>
-			</div>
-		</div>
-	</div>
-</footer>
- 
 
+
+<footer>
+  <div class="container-md">
+      <div class="row">
+        <div id="footer" class="container-fluid p-4 text-white text-center">
+        <nav id="footer-nav">
+         
+            <a href="aboutus.html">About Us</a>
+          </nav>	
+        <span>Funsite für Cineasten by Julia Beck (2022)</span>
+        </div>
+      </div>
+    </div>
+      </div>  
+    </footer>
   </body>
 </html>
